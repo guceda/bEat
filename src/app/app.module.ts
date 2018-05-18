4a03ad0f82421412c95968c46b11fec9a3947e28
Gonzalo Uceda Castro <gon_u@hotmail.com>
2018-05-18 13:22:24 +0200

    creación de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,29 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ExperienciasComponent } from './experiencias/experiencias.component';
+import { EquipoComponent } from './equipo/equipo.component';
+import { ComentariosComponent } from './comentarios/comentarios.component';
+import { IntroComponent } from './intro/intro.component';
+import { ListaExperienciasComponent } from './lista-experiencias/lista-experiencias.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { DetalleExperienciaComponent } from './detalle-experiencia/detalle-experiencia.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ExperienciasComponent,
+    EquipoComponent,
+    ComentariosComponent,
+    IntroComponent,
+    ListaExperienciasComponent,
+    ExperienciaComponent,
+    DetalleExperienciaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/lista-experiencias/lista-experiencias.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-experiencias',
+  templateUrl: './lista-experiencias.component.html',
+  styleUrls: ['./lista-experiencias.component.css']
+})
+export class ListaExperienciasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
